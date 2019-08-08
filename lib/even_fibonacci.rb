e2590ef768715c0f001910a4d4a01e53734de8dd
Matt Williams <matt.seongjun@gmail.com>
2019-08-08 09:26:19 -0400

    Done procedural

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+
+def fibonacci(n, m, lim)
+    if m >= lim
+        return m
+    else
+        return fibonacci(m, n + m, lim)
+    end
+end
+
+puts fibonacci(0, 1, 89)
+puts fibonacci(0, 1, 4000000)
+
+def fibonacci_sum(n, m, sum, lim)
+    if m >= lim
+        return sum
+    else
+        sum = n + m + sum
+        return fibonacci_sum(m, n + m, sum, lim)
+    end
+end
+
+puts fibonacci_sum(0, 1, 0, 89)
+puts fibonacci_sum(0, 1, 0, 4000000)
+
+
